170240651a43ea53b1605d61813daccf3ed90d19
Will Greenberg <greenberg.will@gmail.com>
2022-04-01 14:18:02 -0400

    add seeddata migration

---
+++ LocalBusinessAPI/Migrations/20220401171025_SeedData.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LocalBusinessAPI.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Businesses",
+                columns: new[] { "BusinessId", "Location", "Name", "Type" },
+                values: new object[] { 1, "Over there", "Costco", "Retail" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Businesses",
+                keyColumn: "BusinessId",
+                keyValue: 1);
+        }
+    }
+}
