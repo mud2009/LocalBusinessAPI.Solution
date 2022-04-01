08819c7ead8066199ef11ad702c58a3581516c80
Will Greenberg <greenberg.will@gmail.com>
2022-04-01 15:05:17 -0400

    add businesses id route

--- LocalBusinessAPI/Controllers/BusinessesController.cs
+++ LocalBusinessAPI/Controllers/BusinessesController.cs
@@ -8,21 +8,34 @@ namespace LocalBusinessAPI.Controllers
 {
   [Route("api/[controller]")]
   [ApiController]
-  public class LocalBusinessAPIController : ControllerBase
+  public class BusinessesController : ControllerBase
   {
     private readonly LocalBusinessAPIContext _db;
 
-    public LocalBusinessAPIController(LocalBusinessAPIContext db)
+    public BusinessesController(LocalBusinessAPIContext db)
     {
       _db = db;
     }
-    // GET: api/LocalBusiness
+    // GET: api/Businesses
     [HttpGet]
     public async Task<ActionResult<IEnumerable<Business>>> Get()
     {
       var query = _db.Businesses.AsQueryable();
 
       return await query.ToListAsync();
+    }
+    // GET: api/Businesses/5
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Business>> GetBusiness(int id)
+    {
+        var business = await _db.Businesses.FindAsync(id);
+
+        if (business == null)
+        {
+            return NotFound();
+        }
+
+        return business;
     }
 
   }
