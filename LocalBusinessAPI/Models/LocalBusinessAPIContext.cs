216c9505faba25104ac6e0542eff1c09a2051213
Will Greenberg <greenberg.will@gmail.com>
2022-04-01 17:42:07 -0400

    add more seed data

--- LocalBusinessAPI/Models/LocalBusinessAPIContext.cs
+++ LocalBusinessAPI/Models/LocalBusinessAPIContext.cs
@@ -12,7 +12,16 @@ namespace LocalBusinessAPI.Models
     {
       builder.Entity<Business>()
         .HasData(
-          new Business { BusinessId = 1, Name = "Costco", Type = "Retail", Location = "Over there" }
+          new Business { BusinessId = 1, Name = "Costco", Type = "Retail", Location = "Over there" },
+          new Business { BusinessId = 2, Name = "Mcdonalds", Type = "Fast food", Location = "Over there" },
+          new Business { BusinessId = 3, Name = "Walmart", Type = "Retail", Location = "Over there" },
+          new Business { BusinessId = 4, Name = "Walgreens", Type = "Pharmacy", Location = "Over there" },
+          new Business { BusinessId = 5, Name = "Hardees", Type = "Fast food", Location = "Over there" },
+          new Business { BusinessId = 6, Name = "Armani", Type = "Luxury", Location = "Over there" },
+          new Business { BusinessId = 7, Name = "ToysRus", Type = "Toys", Location = "Over there" },
+          new Business { BusinessId = 8, Name = "Best Buy", Type = "Electronics", Location = "Over there" },
+          new Business { BusinessId = 9, Name = "Lil Caesars", Type = "Pizza", Location = "Over there" },
+          new Business { BusinessId = 10, Name = "Frank's", Type = "Sandwiches", Location = "Over there" }
         );
     }
 
