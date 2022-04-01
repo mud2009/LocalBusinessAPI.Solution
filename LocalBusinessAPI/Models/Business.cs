7b6b579b7db22af2671e97e5bc29dab24116cca3
Will Greenberg <greenberg.will@gmail.com>
2022-04-01 12:21:36 -0400

    add business class and properties

---
+++ LocalBusinessAPI/Models/Business.cs
@@ -0,0 +1,11 @@
+namespace LocalBusinessAPI.Models
+{
+  public class Business
+  {
+    public int BusinessId { get; set; }
+    public string Name { get; set; }
+    public string Type { get; set; }
+    public string Location { get; set; }
+
+  }
+}
