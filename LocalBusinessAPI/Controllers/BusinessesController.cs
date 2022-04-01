c7a48921a5d0b3047c2eff2b87515e6ff794d1b2
Will Greenberg <greenberg.will@gmail.com>
2022-04-01 15:24:40 -0400

    add put method and businessexists method

--- LocalBusinessAPI/Controllers/BusinessesController.cs
+++ LocalBusinessAPI/Controllers/BusinessesController.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -36,6 +38,40 @@ namespace LocalBusinessAPI.Controllers
         }
 
         return business;
+    }
+    // PUT: api/Businesses/5
+    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutBusiness(int id, Business business)
+    {
+        if (id != business.BusinessId)
+        {
+            return BadRequest();
+        }
+
+        _db.Entry(business).State = EntityState.Modified;
+
+        try
+        {
+            await _db.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (!BusinessExists(id))
+            {
+                return NotFound();
+            }
+            else
+            {
+                throw;
+            }
+        }
+
+        return NoContent();
+    }
+    private bool BusinessExists(int id)
+    {
+        return _db.Businesses.Any(e => e.BusinessId == id);
     }
 
   }
